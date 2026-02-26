18f9e752050ff7b44a64396ce28b4c28db33e758
Andrew Appel <appel@princeton.edu>
2026-02-26 10:08:43 -0500

    Improved shape function 1dP1 proofs

--- proof/shape_accuracy.v
+++ proof/shape_accuracy.v
@@ -5,6 +5,7 @@ Require Import vcfloat.VCFloat.
 Require Import Interval.Tactic.
 Import Binary.
 Import Coq.Lists.List ListNotations.
+From LAProof Require Import matrix_model.
 
 
 Set Bullet Behavior "Strict Subproofs".
@@ -16,40 +17,37 @@ Definition _x : ident := 1%positive.  (* Variable name for position *)
 Existing Instance FPCompCert.nans.
 
 Definition x_vmap_list (x : ftype Tdouble) :=
-   [(_x, existT ftype _ x)].
+   [(1%positive, existT ftype _ x)].
 
 Definition x_vmap (x : ftype Tdouble) : valmap :=
  ltac:(make_valmap_of_list (x_vmap_list x)).
 
+(*
+Ltac make_vmap_of_list_float xs :=
+   make_valmap_of_list (map (fun ix => (fst ix, existT ftype _ (snd ix))) (seq.zip (vars (length xs)) xs)).
 
-Check (0.5)%F64 : ftype Tdouble.
-
-(* Definition xbound : R := FT2R((1000)%F64: ftype Tdouble).*)
-Definition xbound : R := 3/4.
-
-Definition x_bmap_list : list varinfo :=
-  [ Build_varinfo Tdouble _x (-xbound) xbound].
+Definition x_vmap (x: ftype Tdouble) :=
+  ltac:(let z := make_vmap_of_list_float [x] in exact z).
 
 Definition x_bmap : boundsmap :=
- ltac:(let z := compute_PTree (boundsmap_of_list x_bmap_list) in exact z).
+ ltac:(let z := compute_PTree (boundsmap_of_list (cuboid_bound [_x])) in exact z).
 
-(*Definition f_1dP1_0 := ltac:(let e' :=
-  HO_reify_float_expr constr:([_x]) (fun x: ftype Tdouble => Norm(0.5 * (1-x)))%F64 in exact e').
 *)
+
+Definition cuboid_bounds (vars: list ident) : list varinfo := map (fun i => Build_varinfo Tdouble i (-1) 1) vars.
+Definition simplex_bounds (vars: list ident) : list varinfo := map (fun i => Build_varinfo Tdouble i 0 1) vars.
+
 Import matrix matrix_util fintype.
 
 Ltac changeStdLibToCore f :=
-let f := eval pattern (@FPStdLib.BMULT FPCompCert.nans FPStdLib.Tdouble) in f in 
-match f with ?A _ => let f := constr:(A (@BMULT FPCompCert.nans Tdouble is_standard_Tdouble)) in 
-let f := eval pattern (@FPStdLib.BMINUS FPCompCert.nans FPStdLib.Tdouble) in f in 
-match f with ?A _ => let f := constr:(A (@BMINUS FPCompCert.nans Tdouble is_standard_Tdouble)) in 
-let f := eval pattern (@FPStdLib.BPLUS FPCompCert.nans FPStdLib.Tdouble) in f in 
-match f with ?A _ => let f := constr:(A (@BPLUS FPCompCert.nans Tdouble is_standard_Tdouble)) in 
-let f := eval pattern (@FPStdLib.BOPP FPCompCert.nans FPStdLib.Tdouble) in f in 
-match f with ?A _ => let f := constr:(A (@BOPP FPCompCert.nans Tdouble is_standard_Tdouble)) in 
-let f := eval cbv beta in f in 
-f
-end end end end.
+ lazymatch f with 
+ | @FPStdLib.BMULT FPCompCert.nans FPStdLib.Tdouble => constr:(@BMULT FPCompCert.nans Tdouble is_standard_Tdouble)
+ | @FPStdLib.BMINUS FPCompCert.nans FPStdLib.Tdouble => constr:(@BMINUS FPCompCert.nans Tdouble is_standard_Tdouble)
+ | @FPStdLib.BPLUS FPCompCert.nans FPStdLib.Tdouble => constr:(@BPLUS FPCompCert.nans Tdouble is_standard_Tdouble)
+ | @FPStdLib.BOPP FPCompCert.nans FPStdLib.Tdouble => constr:(@BOPP FPCompCert.nans Tdouble is_standard_Tdouble)
+ | ?A ?B => let a := changeStdLibToCore A in let b := changeStdLibToCore B in constr:(a b)
+ | _ => constr:(f)
+ end.
 
 Ltac red_to_rowmx f := 
 match f with rowmx_of_list _ => f
@@ -72,69 +70,8 @@ match f with rowmx_of_list ?A =>
  match An with context C [fun_of_matrix (const_mx x) _ ?k] =>
   unify (nat_of_ord k) i;
  let g := context C [x] in 
- let g := constr:(Norm g) in 
   exact g
 end end.
-
-Definition f_1dP1_0 (x: ftype Tdouble) : ftype Tdouble
-  := ltac:(matrix_expression (FShape.θ shapes1dP1F) x 0%nat 0%nat).
-
-Definition f_1dP1_1 (x: ftype Tdouble) : ftype Tdouble
-  := ltac:(matrix_expression (FShape.θ shapes1dP1F) x 0%nat 1%nat).
-
-
-Definition r_1dP1_0 := ltac:(let e' :=
-  HO_reify_float_expr constr:([_x]) f_1dP1_0 in exact e').
-
-
-Definition r_1dP1_1 := ltac:(let e' :=
-  HO_reify_float_expr constr:([_x]) f_1dP1_1 in exact e').
-
-
-Definition acc_1dP1 : R := 2 / IZR (Z.pow_pos 10 16).
-
-Lemma prove_roundoff_bound_1dP1_0:  
-    forall vmap,  prove_roundoff_bound x_bmap vmap r_1dP1_0 acc_1dP1.
-Proof.
-intros.
-prove_roundoff_bound.
--
- prove_rndval; interval.
--
- prove_roundoff_bound2.
- prune_terms (cutoff 30).
- do_interval.
-Qed.
-
-Lemma prove_roundoff_bound_1dP1_1:  
-    forall vmap,  prove_roundoff_bound x_bmap vmap r_1dP1_1 acc_1dP1.
-Proof.
-intros.
-prove_roundoff_bound.
--
- prove_rndval; interval.
--
- prove_roundoff_bound2.
- prune_terms (cutoff 30).
- do_interval.
-Qed.
-
-
-Derive acc_1dP1_0
- in  (forall vmap,  prove_roundoff_bound x_bmap vmap r_1dP1_0 acc_1dP1_0)
- as derive_roundoff_bound_1dP1_0.
-Proof.
-intros.
-prove_roundoff_bound.
--
- prove_rndval; interval.
--
- prove_roundoff_bound2.
- prune_terms (cutoff 30).
- do_interval.
-match goal with |- 0 <= ?acc - ?A => assert (acc = A) by (unfold acc; reflexivity) end.
-lra.
-Qed.
 
 Lemma ord_enum_cases: forall [n] (P: 'I_n -> Prop),
   Forall P (ord_enum n) ->
@@ -144,7 +81,13 @@ intros.
 rewrite Forall_forall in H.
 apply H.
 clear.
-Admitted.
+pose proof @nth_ord_enum' n i i.
+rewrite mv_mathcomp.nth_List_nth in H.
+rewrite <- H.
+apply nth_In.
+rewrite size_ord_enum.
+pose proof (ltn_ord i); lia.
+Qed. 
 
 Ltac is_ground_nat x := lazymatch x with O => idtac | S ?k => is_ground_nat k end.
 
@@ -160,13 +103,13 @@ Ltac compute_ord_enum n :=
   end.
 
 Ltac ord_enum_cases j :=
- match type of j with ordinal ?n => 
+ lazymatch type of j with ordinal ?n => 
   pattern j; 
   apply ord_enum_cases;
   compute_ord_enum n;
   repeat apply Forall_cons; try apply Forall_nil
  end.
- 
+
 Ltac realify := 
 change (@nmodule.Algebra.add _) with Rplus;
 change (@nmodule.Algebra.opp _) with Ropp;
@@ -175,9 +118,72 @@ change (@ssralg.GRing.inv _) with Rinv;
 change (ssralg.GRing.one _) with 1;
 repeat change (nmodule.Algebra.natmul _ ?i) with (INR i).
 
+
+Definition cuboid1_bmap : boundsmap := 
+ ltac:(let z := compute_PTree (boundsmap_of_list (cuboid_bounds [_x])) in exact z).
+
+
+Definition f_1dP1_0 (x: ftype Tdouble) : ftype Tdouble
+  := ltac:(matrix_expression shapes1dP1_float x 0%nat 0%nat).
+
+Definition f_1dP1_1 (x: ftype Tdouble) : ftype Tdouble
+  := ltac:(matrix_expression (FShape.θ shapes1dP1F) x 0%nat 1%nat).
+
+Definition r_1dP1_0 := ltac:(let e' :=
+  HO_reify_float_expr constr:([_x]) f_1dP1_0 in exact e').
+
+Definition r_1dP1_1 := ltac:(let e' :=
+  HO_reify_float_expr constr:([_x]) f_1dP1_1 in exact e').
+
+Definition acc_1dP1 : R := 12 / IZR (Z.pow_pos 10 17).
+
+Lemma prove_roundoff_bound_1dP1_0:  
+    forall vmap,  prove_roundoff_bound cuboid1_bmap vmap r_1dP1_0 acc_1dP1.
+Proof.
+intros.
+prove_roundoff_bound.
+-
+prove_rndval; interval.
+-
+ prove_roundoff_bound2.
+ prune_terms (cutoff 30).
+ do_interval.
+Qed.
+
+Lemma prove_roundoff_bound_1dP1_1:  
+    forall vmap,  prove_roundoff_bound cuboid1_bmap vmap r_1dP1_1 acc_1dP1.
+Proof.
+intros.
+prove_roundoff_bound.
+-
+ prove_rndval; interval.
+-
+ prove_roundoff_bound2.
+ prune_terms (cutoff 30).
+ do_interval.
+Qed.
+
+Derive acc_1dP1_0
+ in  (forall vmap,  prove_roundoff_bound cuboid1_bmap vmap r_1dP1_0 acc_1dP1_0)
+ as derive_roundoff_bound_1dP1_0.
+Proof.
+intros.
+prove_roundoff_bound.
+-
+ prove_rndval; interval.
+-
+ prove_roundoff_bound2.
+ prune_terms (cutoff 30).
+ do_interval.
+match goal with |- 0 <= ?acc - ?A => assert (acc = A) by (unfold acc; reflexivity) end.
+lra.
+Qed.
+
+Check ltac:(ShowBound acc_1dP1_0).
+
 Lemma roundoff_bound_1dP1 : forall (x: 'rV[ftype Tdouble]_1) (j: 'I_2),
        FPCore.is_finite (x ord0 ord0) = true ->
-       Rabs (FT2R (x ord0 ord0)) <= xbound ->
+       -1 <= FT2R (x ord0 ord0) <= 1 ->
        @FPCore.is_finite Tdouble (FShape.θ shapes1dP1F x ord0 j) = true /\
        Rabs (addmx (map_mx (@FT2R Tdouble) (FShape.θ shapes1dP1F x)) 
                     (oppmx (Shape.θ shapes1dP1 (map_mx FT2R x)))
@@ -185,10 +191,9 @@ Lemma roundoff_bound_1dP1 : forall (x: 'rV[ftype Tdouble]_1) (j: 'I_2),
 Proof.
 intros.
 set (x00 := x ord0 ord0) in *.
-assert (boundsmap_denote x_bmap (x_vmap x00)). {
+assert (boundsmap_denote cuboid1_bmap (x_vmap x00)). {
  apply boundsmap_denote_i; simpl; auto.
  eexists. split; [  |split]; auto. reflexivity. split; auto.
- apply Stdlib.Rabs_def2_le; auto.
 }
 ord_enum_cases j; clear j.
 -
